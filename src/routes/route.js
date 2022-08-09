577c8012f3e731b63b8d86cc2c5abda0b8e44442
Sabiha <sabiha.khan@functionup.org>
2022-08-09 11:54:31 +0530

    path params assignment solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,29 +13,69 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
+router.get("/movies/:indexNumber", function(req, res){
+    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    console.log(req.params.indexNumber)
+    let movieIndex = req.params.indexNumber
+    //check index value. less than 0 or greater than array (length - 1) are not valid
+    if(movieIndex<0 || movieIndex>=movies.length) {
+        //if the index is invalid send an error message
+        return res.send('The index value is not correct, Please check the it')
+    }
 
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
+    //if the index was valid send the movie at that index in response
+    let requiredMovie = movies[movieIndex]
+    res.send(requiredMovie)
 })
 
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
+router.get("/films", function(req, res){
+    const films = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       //send all the films
+      res.send(films) 
 })
 
+router.get("/films/:filmId", function(req, res){
+    const films = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+
+       let filmId = req.params.filmId
+
+       //iterate all the films
+       //search for a film whose id matches with the id recevied in request
+       for(let i = 0; i < films.length; i++){
+           let film = films[i]
+           if(film.id == filmId) {
+               //if there is a match return the response from here
+               return res.send(film)
+           }
+       }
+
+       //if there is no match give an error response
+       res.send("The film id doesn't match any movie")
+})
 
 module.exports = router;
 // adding this comment for no reason
