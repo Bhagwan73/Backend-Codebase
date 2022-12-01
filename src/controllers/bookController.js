7d9de58f1cc64a6187c53fd9fd0d715fb00010af
ChavanPriya97 <priya971110@gmail.com>
2022-12-02 00:44:20 +0530

    bookController

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -6,50 +6,54 @@ const { isValidObjectId,isValidISBN,isValidString, isValidDate } = require("../v
 //-------------------------->>-createBook-<<---------------------------<<
 const createBook = async function (req, res) {
     try {
-        let data = req.body
-        const { title, excerpt, userId, ISBN, category, subcategory,releasedAt } = data
+      let data = req.body
+      const { title, excerpt, userId, ISBN, category, subcategory,releasedAt } = data
 
-        //-------------->>-validation-<<----------------<<
-     if(Object.keys(data).length==0){
-         return res.status(400).send({status:false,message:"please provide the data in request"})
-     }
-        //----------->>-title..
-        if (!title) return res.status(400).send({ status: false, message: "title is mandatory in request body" })
-        if(!isValidString(title)) return res.status(400).send({status:false,message:"please provide the valid title"})
-        let uniqueTitle=await bookModel.findOne({title:title})
-        if(uniqueTitle) return res.status(400).send({ status: false, message: "this title is already exists" })
+      //-------------->>-validation-<<----------------<<
+      if(Object.keys(data).length==0) return res.status(400).send({status:false,message:"please provide the data in request"})
+      
+      //----------->>-title..
+      if (!title) return res.status(400).send({ status: false, message: "title is mandatory in request body" })
+      if(!isValidString(title)) return res.status(400).send({status:false,message:"please provide the valid title"})
+      
+      let uniqueTitle=await bookModel.findOne({title:title})
+      if(uniqueTitle) return res.status(400).send({ status: false, message: "this title is already exists" })
 
-        //----------->>-excerpt..
-        if (!excerpt) return res.status(400).send({ status: false, message: "excerpt is mandatory in request body" })
-        if(!isValidString(excerpt)) return res.status(400).send({status:false,message:"please provide the valid excerpt"})
+      //----------->>-excerpt..
+      if (!excerpt) return res.status(400).send({ status: false, message: "excerpt is mandatory in request body" })
+      if(!isValidString(excerpt)) return res.status(400).send({status:false,message:"please provide the valid excerpt"})
 
-        //----------->>-userId..
-        if (!userId) return res.status(400).send({ status: false, message: "userId is mandatory in request body" })
-        if(!isValidObjectId(userId)) return res.status(400).send({status:false,message:"please provide the valid userId"})
-        let ObjectId=await userModel.findById(userId)
-        if(!ObjectId) return res.status(400).send({ status: false, message: "please provide the valid userId" })
+      //----------->>-userId..
+      if (!userId) return res.status(400).send({ status: false, message: "userId is mandatory in request body" })
+      if(!isValidObjectId(userId)) return res.status(400).send({status:false,message:"please provide the valid userId"})
+      
+      let ObjectId=await userModel.findById(userId)
+      if(!ObjectId) return res.status(400).send({ status: false, message: "please provide the valid userId" })
         
-        //----------->>-ISBN..
-        if (!ISBN) return res.status(400).send({ status: false, message: "ISBN is mandatory in request body" })
-        if (!isValidISBN(ISBN)) return res.status(400).send({ status: false, message: "please provide the valid ISBN"})
-        let validISBN= await bookModel.findOne({ISBN:ISBN})
-        if (validISBN)  return res.status(400).send({ status: false, message: "please provide the unique ISBN"})
+      //----------->>-ISBN..
+      if (!ISBN) return res.status(400).send({ status: false, message: "ISBN is mandatory in request body" })
+      if (!isValidISBN(ISBN)) return res.status(400).send({ status: false, message: "please provide the valid ISBN"})
+      
+      let validISBN= await bookModel.findOne({ISBN:ISBN})
+      if (validISBN)  return res.status(400).send({ status: false, message: "please provide the unique ISBN"})
 
-        //----------->>-category..
-        if (!category) return res.status(400).send({ status: false, message: "category is mandatory in request body" })
-        if (!isValidString(category)) return res.status(400).send({ status: false, message: "please provide the valid catagory" })
+      //----------->>-category..
+      if (!category) return res.status(400).send({ status: false, message: "category is mandatory in request body" })
+      if (!isValidString(category)) return res.status(400).send({ status: false, message: "please provide the valid catagory" })
 
-        //----------->>-subcategory..
-        if (!subcategory) return res.status(400).send({ status: false, message: "subcategory is mandatory in request body" })
-        if (!isValidString(subcategory)) return res.status(400).send({ status: false, message: "please provide the valid subcategory" })
+      //----------->>-subcategory..
+      if (!subcategory) return res.status(400).send({ status: false, message: "subcategory is mandatory in request body" })
+      if (!isValidString(subcategory)) return res.status(400).send({ status: false, message: "please provide the valid subcategory" })
 
-        //------------>>-releasedAt..
-        if (!releasedAt) return res.status(400).send({ status: false, message: "releasedAt is mandatory in request body" })
-        if (!isValidDate(releasedAt)) return res.status(400).send({ status: false, message: "please provide the valid releasedAt" })
+      //------------>>-releasedAt..
+    
+      if (!releasedAt) return res.status(400).send({ status: false, message: "releasedAt is mandatory in request body" })
+      if (!isValidDate(releasedAt)) return res.status(400).send({ status: false, message: "please provide the valid releasedAt" })
 
-        //------------->>-createBook..
-        let createBook = await bookModel.create(data)
-        return res.status(201).send({ status: true, data: createBook })
+      //------------->>-createBook..
+      let createBook = await bookModel.create(data)
+      return res.status(201).send({ status: true, data: createBook })
+      
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message })
     }
